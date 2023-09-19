63f2cf87dc1959d2f6e735a4a4b37f662c518c08
M-U-C-K-A <hugo.delacour@univ-rouen.fr>
2023-09-19 10:47:49 +0200

    ajout des classes FEU EAU PLANTE

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Pokemon = /** @class */ (function () {
     function Pokemon(nom, pointsDeVie, pointsAttaque) {
         this.nom = nom;
@@ -23,3 +38,71 @@ var Pokemon = /** @class */ (function () {
     };
     return Pokemon;
 }());
+/*
+(counter | lui même) > le pokémon > contest
+(FEU | EAU) > FEU > PLANTE
+(FEU | PLANTE) > PLANTE > EAU
+(EAU | PLANTE) > EAU > FEU
+*/
+var PokemonFeu = /** @class */ (function (_super) {
+    __extends(PokemonFeu, _super);
+    function PokemonFeu(nom, pointsDeVie, pointsAttaque) {
+        return _super.call(this, nom, pointsDeVie, pointsAttaque) || this;
+    }
+    PokemonFeu.prototype.attaquer = function (adversaire) {
+        if (adversaire instanceof PokemonPlante) {
+            adversaire.subirAttaque(this.pointsAttaque * 2);
+        }
+        else if (adversaire instanceof PokemonEau || adversaire instanceof PokemonFeu) {
+            adversaire.subirAttaque(this.pointsAttaque / 2);
+        }
+        else {
+            adversaire.subirAttaque(this.pointsAttaque);
+        }
+    };
+    return PokemonFeu;
+}(Pokemon));
+var PokemonEau = /** @class */ (function (_super) {
+    __extends(PokemonEau, _super);
+    function PokemonEau(nom, pointsDeVie, pointsAttaque) {
+        return _super.call(this, nom, pointsDeVie, pointsAttaque) || this;
+    }
+    PokemonEau.prototype.attaquer = function (adversaire) {
+        if (adversaire instanceof PokemonFeu) {
+            adversaire.subirAttaque(this.pointsAttaque * 2);
+        }
+        else if (adversaire instanceof PokemonPlante || adversaire instanceof PokemonEau) {
+            adversaire.subirAttaque(this.pointsAttaque / 2);
+        }
+        else {
+            adversaire.subirAttaque(this.pointsAttaque);
+        }
+    };
+    return PokemonEau;
+}(Pokemon));
+var PokemonPlante = /** @class */ (function (_super) {
+    __extends(PokemonPlante, _super);
+    function PokemonPlante(nom, pointsDeVie, pointsAttaque) {
+        return _super.call(this, nom, pointsDeVie, pointsAttaque) || this;
+    }
+    PokemonPlante.prototype.attaquer = function (adversaire) {
+        if (adversaire instanceof PokemonEau) {
+            adversaire.subirAttaque(this.pointsAttaque * 2);
+        }
+        else if (adversaire instanceof PokemonFeu || adversaire instanceof PokemonPlante) {
+            adversaire.subirAttaque(this.pointsAttaque / 2);
+        }
+        else {
+            adversaire.subirAttaque(this.pointsAttaque);
+        }
+    };
+    return PokemonPlante;
+}(Pokemon));
+var pikachu = new PokemonEau("Pikachu", 100, 30);
+var bulbasaur = new PokemonPlante("Bulbasaur", 100, 25);
+console.log('bulbasaur avant : ');
+bulbasaur.afficherInformations();
+console.log('');
+pikachu.attaquer(bulbasaur);
+console.log('bulbasaur apres : ');
+bulbasaur.afficherInformations();
--- script.ts
+++ script.ts
@@ -4,29 +4,82 @@ class Pokemon {
   protected pointsAttaque: number;
 
   constructor(nom: string, pointsDeVie: number, pointsAttaque: number) {
-      this.nom = nom;
-      this.pointsDeVie = pointsDeVie;
-      this.pointsAttaque = pointsAttaque;
+    this.nom = nom;
+    this.pointsDeVie = pointsDeVie;
+    this.pointsAttaque = pointsAttaque;
   }
 
   estMort(): boolean {
-      return this.pointsDeVie <= 0;
+    return this.pointsDeVie <= 0;
   }
 
   attaquer(adversaire: Pokemon): void {
-      adversaire.subirAttaque(this.pointsAttaque);
+    adversaire.subirAttaque(this.pointsAttaque);
   }
 
   afficherInformations(): void {
-      console.log(`Nom: ${this.nom}`);
-      console.log(`Points de Vie: ${this.pointsDeVie}`);
-      console.log(`Points d'Attaque: ${this.pointsAttaque}`);
+    console.log(`Nom: ${this.nom}`);
+    console.log(`Points de Vie: ${this.pointsDeVie}`);
+    console.log(`Points d'Attaque: ${this.pointsAttaque}`);
   }
 
   public subirAttaque(degats: number): void {
-      this.pointsDeVie -= degats;
-      if (this.pointsDeVie < 0) {
-          this.pointsDeVie = 0;
-      }
+    this.pointsDeVie -= degats;
+    if (this.pointsDeVie < 0) {
+      this.pointsDeVie = 0;
+    }
+  }
+}
+/*
+(counter | lui même) > le pokémon > contest  
+(FEU | EAU) > FEU > PLANTE
+(FEU | PLANTE) > PLANTE > EAU
+(EAU | PLANTE) > EAU > FEU
+*/
+class PokemonFeu extends Pokemon {
+  constructor(nom: string, pointsDeVie: number, pointsAttaque: number) {
+    super(nom, pointsDeVie, pointsAttaque);
+  }
+
+  attaquer(adversaire: Pokemon): void {
+    if (adversaire instanceof PokemonPlante) {
+      adversaire.subirAttaque(this.pointsAttaque * 2);
+    } else if (adversaire instanceof PokemonEau || adversaire instanceof PokemonFeu) {
+      adversaire.subirAttaque(this.pointsAttaque / 2);
+    } else {
+      adversaire.subirAttaque(this.pointsAttaque);
+    }
+  }
+}
+
+class PokemonEau extends Pokemon {
+  constructor(nom: string, pointsDeVie: number, pointsAttaque: number) {
+    super(nom, pointsDeVie, pointsAttaque);
+  }
+
+  attaquer(adversaire: Pokemon): void {
+    if (adversaire instanceof PokemonFeu) {
+      adversaire.subirAttaque(this.pointsAttaque * 2);
+    } else if (adversaire instanceof PokemonPlante || adversaire instanceof PokemonEau) {
+      adversaire.subirAttaque(this.pointsAttaque / 2);
+    } else {
+      adversaire.subirAttaque(this.pointsAttaque);
+    }
+  }
+}
+
+class PokemonPlante extends Pokemon {
+  constructor(nom: string, pointsDeVie: number, pointsAttaque: number) {
+    super(nom, pointsDeVie, pointsAttaque);
+  }
+
+  attaquer(adversaire: Pokemon): void {
+    if (adversaire instanceof PokemonEau) {
+      adversaire.subirAttaque(this.pointsAttaque * 2);
+    } else if (adversaire instanceof PokemonFeu || adversaire instanceof PokemonPlante) {
+      adversaire.subirAttaque(this.pointsAttaque / 2);
+    } else {
+      adversaire.subirAttaque(this.pointsAttaque);
+    }
   }
 }
